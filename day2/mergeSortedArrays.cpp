4d13ac8cc36763faf8b0502cac33b51ae432882d
Putta Bhanu Prakash <puprakash@merkleinc.com>
2021-09-25 17:14:28 +0530

    Adding day2

---
+++ day2/mergeSortedArrays.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+// this method works, but in interview this wouldnt work,
+// they will need a solution that is not using sort function
+void mergeSortedArrays(vector<int> &nums1, int m, vector<int> &nums2, int n){
+    for(int i=m; i<m+n; i++){
+        nums1[i] = nums2[i-m];
+    }
+    sort(nums1.begin(), nums1.end());
+}
+
+// This method will work only when all 
+// the elements of both arrays are positive
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    if(m==0&&n!=0){
+        nums1 = nums2;
+        return;
+    }else if(m!=0&&n==0){
+        return;
+    }
+    
+    int l1=0, l2=0, s1=m, s2=n;
+    while(l2<n && l1<m+n){
+        // If n1 <= n2
+        // increase l1
+        // else n1 > n2
+        // move n1 elements 
+        // to the right by
+        // x places and insert
+        // n2 in n1
+        if(nums1[l1] <= nums2[l2]&&nums1[l1]!=0){
+            l1++;
+            s1--;
+        }else{
+            for(int j=l1+s1; j>l1; j--){
+                cout<<"j:"<<j<<", l1:"<<l1<<", nums1[j]:"<<nums1[j]<<endl;
+                nums1[j] = nums1[j-1];
+                cout<<"j:"<<j<<", l1:"<<l1<<", nums1[j]:"<<nums1[j]<<endl;
+            }
+            cout<<"nums1[l1]:"<<nums1[l1]<<endl;
+            nums1[l1] = nums2[l2];
+            cout<<"nums1[l1]:"<<nums1[l1]<<endl;
+            l1++;
+            l2++;
+            s2--;
+        }
+    }
+}
---
+++ day2/twoSum.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// given a sorted array
+// and a target element
+// return a vector containing
+// the indices of the 2 elements 
+// whose sum is equal to target
+
+vector<int> twoSum(vector<int> nums, int target){
+    vector<int> ans(2);
+    for(int i=0; i<nums.size(); i++){
+        for(int j=i+1; j<nums.size(); j++){
+            if(nums[i] + nums[j] == target){
+                ans[0] = i;
+                ans[1] = j;
+                return ans;
+            }
+        }
+    }
+return ans;
+}
+
+// this method will only work when
+// the given array is sorted already
+vector<int> twoSum(vector<int> nums, int target){
+    int l=0, r=nums.size()-1;
+    vector<int> ans(2);
+    while(l<r){
+        int sum = nums[l]+nums[r];
+        if(sum>target){
+            r--;
+        }else if(sum<target){
+            l++;
+        }else{
+            ans[0] = l;
+            ans[1] = r;
+            break;
+        }
+    }
+return ans;
+}
