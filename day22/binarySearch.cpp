0989674313c30b6636a613f5d6a6ee56e219d134
Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-10-16 16:49:18 +0530

    Added Day22

--- day22/binarySearch.cpp
+++ day22/binarySearch.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-int search(vector<int>& nums, int target) {
+int binarySearch(vector<int>& nums, int target) {
     int l=0, r=nums.size()-1;
     while(l<=r){
         int mid = l+ (r-l)/2;
