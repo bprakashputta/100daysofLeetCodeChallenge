37425874962553b1cfd7efad5cd780494338ac5d
Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-11-09 22:00:50 +0530

    Frequency of Digits in String

---
+++ day47/frequencyofdigitsinstring.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    char str[1000];
+    scanf("%s", &str);
+
+    int freq[10];
+    for(int i=0; i<=9; i++){
+        freq[i]=0;
+    }
+    int len = 0;
+    for(int i=0; str[i]!='\0'; i++){
+        len++;
+    }
+
+    for(int i=0; i<len; i++){
+        if(str[i]>='0'&&str[i]<='9'){
+            freq[str[i]-'0']++;
+        }
+    }
+    // print frequency of characters
+    for(int i=0; i<=9; i++){
+        printf("%d ",freq[i]);
+    }
+    printf("\n");
+return 0;
+}
