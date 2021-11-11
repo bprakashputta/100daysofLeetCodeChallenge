46a77b1ad1bfd2bb677ab64671731438e95aed87
Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-11-11 22:08:51 +0530

    Added Dy49

---
+++ day49/minimumoperationstomakearrayequal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            arr[i] = (2*i)+1;
+        }
+        int num = 0;
+        if(n%2==0){
+            num = arr[n/2]-1;
+        }else{
+            num = arr[n/2];
+        }
+        int ops=0;
+        for(int i=0; i<n/2; i++){
+            ops += num - arr[i];
+        }
+    return ops;
+    }
+};
