10cdb276d1b899d27603337e36ff89a89ebb419c
Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-11-11 22:12:54 +0530

    Added Dy49

--- day49/minimumoperationstomakearrayequal.cpp
+++ day49/minimumoperationstomakearrayequal.cpp
@@ -17,4 +17,27 @@ public:
         }
     return ops;
     }
+
+    // Without using extra space of array
+    int minOperations(int n) {
+        // vector<int> arr(n);
+        // for(int i=0; i<n; i++){
+        //     arr[i] = (2*i)+1;
+        // }
+        int num = 0;
+        if(n%2==0){
+            // num = arr[n/2]-1;
+            num = 2*(n/2);
+        }else{
+            // num = arr[n/2];
+            num = 2*(n/2)+1;
+        }
+        int ops=0;
+        for(int i=0; i<n/2; i++){
+            // ops += num - arr[i];
+            ops += num - (2*i)-1;
+        }
+    return ops;
+    }
+
 };
