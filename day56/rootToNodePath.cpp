b6984d7718166a2bd7917f40f2cc5dd4a447cb29
Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-11-18 16:25:34 +0530

    Added Day56

---
+++ day56/rootToNodePath.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct TreeNode{
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+bool rootToNodePath(TreeNode* root, int x, vector<int> &path){
+    if(root==NULL){
+        return false;
+    }
+
+    path.push_back(root->data);
+    if(root->data == x){
+        path.push_back(x);
+        return true;
+    }
+    if(rootToNodePath(root->left, x, path) || rootToNodePath(root->right, x, path)){
+        return true;
+    }
+    path.pop_back();
+    
+return false;
+}
+
+int main(){
+    for(int i=0; i<)
+}
