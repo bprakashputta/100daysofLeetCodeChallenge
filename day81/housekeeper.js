f8e92afc7a6559d9689f6ae3bb9093c8fe7b1152
Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-12-13 18:41:57 +0530

    Added Day81

--- day81/housekeeper.js
+++ day81/housekeeper.js
@@ -1,9 +1,17 @@
-function HouseKeeper(name, mobile, start) {
-    this.name = name;
-    this.mobile = mobile;
-    this.start = start;
+class HouseKeeper {
+    constructor(name, mobile, start) {
+        this.name = name;
+        this.mobile = mobile;
+        this.start = start;
+        this.cleanRoom = function() {
+            clean();
+        }
+    }
 }
 
+function clean() {
+    alert('Cleaning in progress!');
+}
 // let houseKeeper = {
 //     name: "Angel",
 //     mobile: "8786817978",
@@ -11,3 +19,4 @@ function HouseKeeper(name, mobile, start) {
 // }
 
 let houseKeeper = new HouseKeeper("Angel", "8786817978", "19/09/2020");
+houseKeeper.cleanRoom();
