e70e0d502152a5b6c612ab9004cb5603c81f71e0
Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-12-13 18:41:36 +0530

    Added Day81

--- day81/DrumKit/index.js
+++ day81/DrumKit/index.js
@@ -55,4 +55,66 @@ function handleClick() {
 
     // let audio = new Audio('./sounds/tom-2.mp3');
     // audio.play();
+
+    // animate button now
+    buttonAnimation(buttonEvent);
+    console.log(":hi")
+}
+
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+
+    console.log(key);
+    let audio;
+    switch (key) {
+        case 'w':
+            audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'a':
+            audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 's':
+            audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 'd':
+            audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'j':
+            audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case 'k':
+            audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'l':
+            audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+
+        default:
+            console.log("No event")
+
+    }
+
+    // let audio = new Audio('./sounds/tom-2.mp3');
+    // audio.play();
+}
+
+function buttonAnimation(actionKey) {
+    console.log(actionKey);
+    let activeButton = document.querySelector("." + actionKey);
+    console.log(activeButton)
+    activeButton.classList.add('pressed');
+    setTimeout(function() {
+        activeButton.classList.remove('pressed');
+    }, 100);
 }
