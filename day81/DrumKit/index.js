e8f92b3d352f9103caa65d678d3ed692340a917c
Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-12-13 16:25:50 +0530

    Added Day81

--- day81/DrumKit/index.js
+++ day81/DrumKit/index.js
@@ -14,5 +14,45 @@ for (let i = 0; i < numberofDrumButtons; i++) {
 }
 
 function handleClick() {
-    alert('I am clicked');
+    // alert('I am clicked');
+    let buttonEvent = this.innerHTML;
+    console.log(buttonEvent);
+    let audio;
+    switch (buttonEvent) {
+        case 'w':
+            audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'a':
+            audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 's':
+            audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 'd':
+            audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'j':
+            audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case 'k':
+            audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'l':
+            audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+
+        default:
+            console.log("No event")
+
+    }
+
+    // let audio = new Audio('./sounds/tom-2.mp3');
+    // audio.play();
 }
